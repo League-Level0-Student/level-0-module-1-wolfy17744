7e84bdef8ac4d1b74befab44397430057011518e
mjfre <league@wts22.attlocal.net>
2017-11-17 16:56:46 -0800

    yee

--- src/SecretMessageBox.java
+++ src/SecretMessageBox.java
@@ -1,31 +1,43 @@
+
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
-
 
 import javax.swing.JOptionPane;
 
 /**
  * Secret Message Box / Secure Messaging System
  * 
- * You want to leave a message on one of the Mac computers so that only your friend can read it.
- * You set up the passcode and the secret message. 
- * Your friend types in the passcode to retrieve the message.
+ * You want to leave a message on one of the Mac computers so that only your
+ * friend can read it. You set up the passcode and the secret message. Your
+ * friend types in the passcode to retrieve the message.
  * 
  */
 
 public class SecretMessageBox {
+	public static void main(String[] args) {
 
-	// 0. Make a main method and put steps 1-5 inside it
-	
-	// 1. Set a password in a String variable
-	
-	// 2. Using a pop-up, ask the first person for a secret message and store it in a variable
+		// 0. Make a main method and put steps 1-5 inside it
 
-	// 3. Now use a pop-up to tell the NEXT user that they can only see the secret message 
-	//    if they can guess the passcode
+		// 1. Set a password in a String variable
+		String password = "yee";
+		// 2. Using a pop-up, ask the first person for a secret message and store it in
+		// a variable
+		String message = JOptionPane.showInputDialog("MESSAGE");
+		// 3. Now use a pop-up to tell the NEXT user that they can only see the secret
+		// message
+		// if they can guess the passcode
+		String gPasscode = JOptionPane.showInputDialog(null,
+				"you can only see the message if you can guess the passcode");
+		// 4. If their guess matches the password, show them the secret message
+		if (gPasscode.equals(password)) {
+			JOptionPane.showMessageDialog(null, message);
+		} else {
+			for (int i = 0; i < 100; i++) {
+				JOptionPane.showMessageDialog(null, "INTRUDER!!", "EEEEEEE", JOptionPane.WARNING_MESSAGE);
+			}
 
-	// 4. If their guess matches the password, show them the secret message
+		}
+		// 5. If the password does not match, pop-up "INTRUDER!!"
 
-	// 5. If the password does not match, pop-up "INTRUDER!!"
-
+	}
 }
--- src/TheRiddler.java
+++ src/TheRiddler.java
@@ -1,24 +1,50 @@
+import javax.swing.JOptionPane;
+
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
-
-
 
 public class TheRiddler {
 
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-
+		double score = 0;
+		int riddles = 0;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
-		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-
-		// 5. Otherwise, say "wrong" and tell them the answer
-
+		String ans1 = JOptionPane.showInputDialog("WHATS IN THE BOX");
+		// 4. If they got the answer right, pop up "correct!" and increase the score by
+		// one
+		if (ans1.equals("nothing")) {
+			JOptionPane.showMessageDialog(null, "CORRECT");
+			score = score + 1;
+			riddles++;
+		} else {
+			// 5. Otherwise, say "wrong" and tell them the answer
+			JOptionPane.showMessageDialog(null, "sucess", "ERROR", JOptionPane.ERROR_MESSAGE);
+			riddles++;
+		}
 		// 6. Add some more riddles
-
+		String ans2 = JOptionPane.showInputDialog("what is the question");
+		if (ans2.equals("this") || ans2.equals("WHATS IN THE BOX") || ans2.equals("the question")) {
+			JOptionPane.showMessageDialog(null, "CORRECT");
+			score = score + 1;
+			riddles++;
+		} else {
+			JOptionPane.showMessageDialog(null, "IT IS IN THE BOX", "", JOptionPane.ERROR_MESSAGE);
+			riddles++;
+		}
+		String ans3 = JOptionPane.showInputDialog("what smells like red paint and is blue");
+		if (ans3.equals("blue paint")) {
+			JOptionPane.showMessageDialog(null, "CORRECT");
+			score = score + 1;
+			riddles++;
+		} else {
+			JOptionPane.showMessageDialog(null, "the answer was blue paint", "CRITICAL FAILIURE",
+					JOptionPane.ERROR_MESSAGE);
+			riddles++;
+		}
 		// 2. Make a pop up to show the score.
-		
+		JOptionPane.showMessageDialog(null, score / riddles * 100 + "%", "Your score is...", JOptionPane.YES_NO_OPTION);
+
 	}
 }
-
